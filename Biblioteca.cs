e2f0b9907e216baa2364eeda421b8298ebb91fe1
Enrico-Antonio-Signore <enricosignore00@gmail.com>
2021-11-12 09:10:50 +0100

    Creazione Biblioteca e metodi

---
+++ Biblioteca.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Verifica
+{
+    class Biblioteca
+    {
+        public List<Libro> listalibri;
+        private string _nome;
+        private string _indirizzo;
+        private int _apertura;
+        private int _chiusura;
+        public Biblioteca(string nome, string indirizzo, string apertura)
+        {
+            listalibri = new List<Libro>();
+        }
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                _nome = value;
+            }
+        }
+        public string Indirizzo
+        {
+            get
+            {
+                return _indirizzo;
+            }
+            set
+            {
+                _indirizzo = value;
+            }
+        }
+        public int Apertura
+        {
+            get
+            {
+                return _apertura;
+            }
+            set
+            {
+                _apertura = value;
+            }
+        }
+        public int Chiusura
+        {
+            get
+            {
+                return _chiusura;
+            }
+            set
+            {
+                _chiusura = value;
+            }
+        }
+    }
+}
